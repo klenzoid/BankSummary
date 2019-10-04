51267e883442fadd584beffd5eec28a97ef1041b
jp-sdev <jon.pablo80@gmail.com>
2019-10-04 13:27:55 -0400

    Updates

---
+++ precache-manifest.85bb0601b46077617d1f933cbddac25b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e159e3e9431e0ecf3a0aa8a104b8f15f",
+    "url": "/bank-summary/index.html"
+  },
+  {
+    "revision": "7188786cce9878b6aa1f",
+    "url": "/bank-summary/static/css/main.80bea198.chunk.css"
+  },
+  {
+    "revision": "4c63ede992cf2f582740",
+    "url": "/bank-summary/static/js/2.d27bfc3f.chunk.js"
+  },
+  {
+    "revision": "7188786cce9878b6aa1f",
+    "url": "/bank-summary/static/js/main.d97f413a.chunk.js"
+  },
+  {
+    "revision": "1efa1e76122d2110d9ab",
+    "url": "/bank-summary/static/js/runtime~main.0037f9b8.js"
+  }
+]);
